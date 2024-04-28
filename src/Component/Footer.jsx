63828e6dffae70360cb11e806fd5b3c861160895
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-04-29 00:48:27 +0530

    added code

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,14 @@
 const Footer = ()=>{
     return (
-        <>
-        <p>This is Footer Section</p>
-        </>
+        <>  
+        <div className=" bg-gray-200 text-center p-2 font-semibold">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, sequi?
+            </p>
+        </div>     
+          <p className="text-center text-blue-700">
+           <a href=""><u>Powored Bu website.com</u></a> 
+          </p>
+         </>
     );
 }
 export default Footer;
